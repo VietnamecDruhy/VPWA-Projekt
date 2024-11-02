3231252328b8be0f3a27548bdd691bbc6fb38b61
VietnamecDruhy <113976797+VietnamecDruhy@users.noreply.github.com>
2024-11-02 14:59:42 +0100

    set up 2

--- VPWA-ping-server/config/cors.ts
+++ VPWA-ping-server/config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: true,
+  origin: ['http://localhost:9000'],
 
   /*
   |--------------------------------------------------------------------------
@@ -56,7 +56,8 @@ const corsConfig: CorsConfig = {
   |
   | Following is the list of default methods. Feel free to add more.
   */
-  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'],
+
 
   /*
   |--------------------------------------------------------------------------
